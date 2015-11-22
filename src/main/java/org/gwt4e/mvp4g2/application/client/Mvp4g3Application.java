f91fb286ddd6ee958e4dbe0c6d110696f91ba23d
Frank Hossfeld <frank.hossfeld@googlemail.com>
2015-11-22 11:37:28 +0100

    SetUp project

---
+++ src/main/java/org/gwt4e/mvp4g2/application/client/Mvp4g3Application.java
@@ -0,0 +1,12 @@
+package org.gwt4e.mvp4g2.application.client;
+
+import com.google.gwt.user.client.ui.HasWidgets;
+
+/**
+ * Created by hoss on 21.08.15.
+ */
+public interface Mvp4g3Application {
+
+  void run(HasWidgets.ForIsWidget viewPort);
+
+}
