f2b34c241f676393255d8f3601207d0fa125edac
Frank Hossfeld <frank.hossfeld@googlemail.com>
2017-11-25 12:10:44 +0100

    remove old code & backups

--- etc/back/mvp4g2/core/processor/Mvp4g2ClassNames.java
+++
@@ -1,7 +0,0 @@
-package de.gishmo.gwt.mvp4g2.old.processor;
-
-public class Mvp4g2ClassNames {
-
-  public static final String APPLICATION = "de.gishmo.gwt.mvp4g2.core.client.annotation.Application";
-
-}
--- old/gwt/mvp4g/client/Context.java
+++
@@ -1,8 +0,0 @@
-package gwt.mvp4g.client;
-
-/**
- * Created by hoss on 19.09.16.
- */
-public class Context {
-// Todo EventBus uns so was rein
-}
