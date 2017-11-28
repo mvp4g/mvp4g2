ce9c60d1067ca735b84000d95fa9051444ca1eea
Frank Hossfeld <frank.hossfeld@googlemail.com>
2017-11-28 07:00:02 +0100

    more test

--- mvp4g2-processor/src/test/resources/de/gishmo/gwt/mvp4g2/processor/mock/MockEventBus.java
+++ mvp4g2-processor/src/test/resources/de/gishmo/gwt/mvp4g2/processor/mock/MockEventBus.java
@@ -1,8 +1,8 @@
 /*
- * Copyright (C) 2016 Frank Hossfeld
+ * Copyright (C) 2016 Frank Hossfeld <frank.hossfeld@googlemail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ *  you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
@@ -12,23 +12,21 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
 
-package de.gishmo.gwt.mvp4g2.processor.event;
+package de.gishmo.gwt.mvp4g2.processor.mock;
 
 import de.gishmo.gwt.mvp4g2.client.eventbus.IsEventBus;
 import de.gishmo.gwt.mvp4g2.client.eventbus.annotation.Event;
-import de.gishmo.gwt.mvp4g2.client.eventbus.annotation.EventBus;
-import de.gishmo.gwt.mvp4g2.client.eventbus.annotation.Start;
-import de.gishmo.gwt.mvp4g2.processor.mock.MockOneEventHandler;
-import de.gishmo.gwt.mvp4g2.processor.mock.MockShellPresenter;
+import de.gishmo.gwt.mvp4g2.client.ui.IsEventHandler;
+import de.gishmo.gwt.mvp4g2.client.ui.IsShell;
+import de.gishmo.gwt.mvp4g2.client.ui.AbstractEventHandler;
 
-@EventBus(shell = MockShellPresenter.class)
-public interface StartEventTestEventBusWithOneStartAnnotation
+public interface MockEventBus
   extends IsEventBus {
 
-  @Start
   @Event(handlers = MockOneEventHandler.class)
-  public void start();
-
+  public void onOneEvent() {
+  }
 }
