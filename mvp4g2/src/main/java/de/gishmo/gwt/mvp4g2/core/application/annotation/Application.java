57dad24944ff6926044542f088a9ce99b2c09abd
Frank Hossfeld <frank.hossfeld@googlemail.com>
2018-02-05 05:48:56 +0100

    update documentation

--- mvp4g2/src/main/java/de/gishmo/gwt/mvp4g2/core/application/annotation/Application.java
+++ mvp4g2/src/main/java/de/gishmo/gwt/mvp4g2/core/application/annotation/Application.java
@@ -14,7 +14,7 @@ import java.lang.annotation.RetentionPolicy;
  * <ul>
  * <li>eventBus: defines the eventbus of this application</li>
  * <li>loader: a loader that will be executed in case the application loads. If no loader
- * is defined, the NoApplicationLoader.class will be used</li>
+ * is defined, the NoApplicationLoader.class will be used. In this case, the loader will do nothing.</li>
  * </ul>
  *
  * @author Frank Hossfeld
